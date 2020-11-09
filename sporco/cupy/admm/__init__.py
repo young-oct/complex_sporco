fc4b36b41ca1c10a03865bdaf1367e0b92d94ae5
junzhe.wang <junzhe.wang@audiopticsmedical.com>
2020-11-09 15:14:14 -0400

    complex type update

--- sporco/cupy/admm/__init__.py
+++ sporco/cupy/admm/__init__.py
@@ -56,6 +56,10 @@ def _update_rho(self, k, r, s):
                 rsf = rhomlt
             elif s > (mu / xi) * r:
                 rsf = 1.0 / rhomlt
+        if rsf.dtype == complex:
+            self.rho *= complex(rsf)
+            self.U /= rsf
+        else:
             self.rho *= float(rsf)
             self.U /= rsf
             if rsf != 1.0:
